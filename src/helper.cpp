624d286fedc92e92b7050a1859fe280af88aff30
Zira project <ziracms@gmail.com>
2020-10-31 00:10:13 +0500

    ver.1.10.0

--- src/helper.cpp
+++ src/helper.cpp
@@ -29,7 +29,7 @@
 #endif
 
 const QString APPLICATION_NAME = "Zira Editor";
-const QString APPLICATION_VERSION = "1.9.9";
+const QString APPLICATION_VERSION = "1.10.0";
 const QString ORGANIZATION_NAME = "Zira";
 const QString PROJECT_NAME = "Zira project";
 const QString AUTHOR_EMAIL_USERNAME = "ziracms";
